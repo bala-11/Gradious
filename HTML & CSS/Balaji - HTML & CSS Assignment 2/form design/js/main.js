2528a8d460caf3ae14609432c6673b3fdb12e548
Balaji <balaji.kalimuthu@mphasis.com>
2023-12-07 14:57:57 +0530

    formatted

--- HTML & CSS/Balaji - HTML & CSS Assignment 2/form design/js/main.js
+++ HTML & CSS/Balaji - HTML & CSS Assignment 2/form design/js/main.js
@@ -1,40 +1,61 @@
+function validate() {
+  var firstName = document.getElementById("firstname").value;
+  var lastName = document.getElementById("lastname").value;
+  var phoneNumber = document.getElementById("phonenumber").value;
+  var emailId = document.getElementById("email").value;
+  var applyPosition = document.getElementById("applyPos").value;
+  var personGender = document.querySelector(
+    'input[name="Gender"]:checked'
+  ).value;
+  var city = document.getElementById("dropdown").value;
+  var earlyDate = document.getElementById("earliestPossibleDate").value;
+  var preferDate = document.getElementById("preferInterviewDate").value;
 
-function validate(){
-    var firstName = document.getElementById("firstname").value;
-    var lastName = document.getElementById("lastname").value;
-    var phoneNumber = document.getElementById("phonenumber").value;
-    var emailId = document.getElementById("email").value;
-    var applyPosition = document.getElementById("applyPos").value;
-    var personGender = document.querySelector('input[name="Gender"]:checked').value;
-    var city = document.getElementById("dropdown").value;
-    var earlyDate = document.getElementById("earliestPossibleDate").value;
-    var preferDate = document.getElementById("preferInterviewDate").value;
+  console.log(
+    firstName,
+    lastName,
+    phoneNumber,
+    emailId,
+    personGender,
+    applyPosition,
+    city,
+    earlyDate,
+    preferDate
+  );
 
-    console.log(firstName,lastName,phoneNumber,emailId,personGender,applyPosition,city,earlyDate,preferDate);    
+  var regName = /^[A-Za-z][A-Za-z_]{0,29}$/;
+  var regPhn = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/;
+  var regEmail = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
 
-    var regName  = /^[A-Za-z][A-Za-z_]{0,29}$/ 
-    var regPhn =  /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/
-    var regEmail = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/
-
-
-
-
-     if(regName.test(firstName) && regName.test(lastName) && regPhn.test(phoneNumber) && regEmail.test(emailId) && regName.test(applyPosition)){
-        localStorage.setItem("Firstname", firstName);
-        localStorage.setItem("Lastname", lastName);
-        localStorage.setItem("Email Id", emailId);
-        localStorage.setItem("Phone Number", phoneNumber);
-        localStorage.setItem("Applied Position", applyPosition);
-        localStorage.setItem("City",city);
-        localStorage.setItem("Gender" , personGender);
-        localStorage.setItem("Prefered Interview Date", preferDate);
-        localStorage.setItem("Earliest Possible Start Date",earlyDate);
-        alert("You have successfully applied for full stack developer...");
-     }
-     else{
-        console.log(firstName,lastName,phoneNumber,emailId,personGender,applyPosition,city,earlyDate,preferDate);
-        alert("Invalid Credentials");
-     }
-
-
+  if (
+    regName.test(firstName) &&
+    regName.test(lastName) &&
+    regPhn.test(phoneNumber) &&
+    regEmail.test(emailId) &&
+    regName.test(applyPosition)
+  ) {
+    localStorage.setItem("Firstname", firstName);
+    localStorage.setItem("Lastname", lastName);
+    localStorage.setItem("Email Id", emailId);
+    localStorage.setItem("Phone Number", phoneNumber);
+    localStorage.setItem("Applied Position", applyPosition);
+    localStorage.setItem("City", city);
+    localStorage.setItem("Gender", personGender);
+    localStorage.setItem("Prefered Interview Date", preferDate);
+    localStorage.setItem("Earliest Possible Start Date", earlyDate);
+    alert("You have successfully applied for full stack developer...");
+  } else {
+    console.log(
+      firstName,
+      lastName,
+      phoneNumber,
+      emailId,
+      personGender,
+      applyPosition,
+      city,
+      earlyDate,
+      preferDate
+    );
+    alert("Invalid Credentials");
+  }
 }
