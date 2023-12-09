c556d1100d61e8a4133fafcb230d321024583184
Balaji <balaji.kalimuthu@mphasis.com>
2023-12-09 13:24:48 +0530

    javascript practice

---
+++ Javascript programs/array.js
@@ -0,0 +1,153 @@
+
+/**
+ *  Copy one array into another
+ */ 
+
+var arr1 = [1,2,3,4,5];
+var arr2 =[...arr1];
+console.log("Copy one array into another:",...arr2);
+
+/**
+ *     Frequency of each element in the array
+ */ 
+
+var arr = [1,3,4,5,66,7,3,7,7,4,5,6,7];
+var map = new Map();
+
+for(let i=0;i<arr.length;i++){
+    if(map.has(arr[i])){
+        let count = map.get(arr[i]);
+        map.set(arr[i],count + 1);
+    }
+    else{
+        map.set(arr[i],1);
+    }
+}
+
+var max = 0;
+var ans = 0;
+for(let [key,value] of map.entries()){
+    if(value > max){
+        max = value;
+        ans = key ;
+    }
+}
+
+console.log("Highest Frequency of each element in the array:",ans);
+
+
+/**
+ *     Left rotate the array
+ */  
+
+var arr3 =  [1,2,3,4,5,6,7,8,9,10];
+
+let k = 2;
+let n = arr3.length
+let p=0;
+var temp = [];
+let i=n-1;
+while(i>n-k-1){
+    temp[p] = arr3[i];
+    p++;
+    i--;
+}
+
+let m = n-k-1;
+while(m>=0){
+    arr3[m+k] = arr3[m];
+    m--;
+}
+
+let z=0;
+while(z<k){
+    arr3[z] = temp[z]
+    z++;
+}
+console.log("Left rotate the array by K :",...arr3);
+
+/**
+ *  Reverse the number with functions
+ */
+
+let number = 23582378;
+console.log("Reverse the number with functions:",parseInt(number.toString().split("").reverse().join("")));
+
+/** 
+ * Reverse number without functions
+ * 
+*/
+
+/**
+ * Print Duplicates in the array 
+ */
+
+var duplicatesArray = [1,2,3,4,5,6,7,7,4,1,2,3,4,8,8,3,2,1,7,4,7];
+var dupArray = [];
+
+for(let i=0;i<duplicatesArray.length;i++){
+    for(let j=i+1;j<duplicatesArray.length;j++){
+        if(duplicatesArray[i] == duplicatesArray[j]){
+            if(dupArray.includes(duplicatesArray[i])){
+                continue;
+            }
+            else{
+                dupArray.push(duplicatesArray[i]);
+            }
+        }
+    }
+}
+
+console.log("Duplicates array : ", ...dupArray);
+
+/**
+ * Print even positions in the array 
+ */
+
+var even = [1,2,3,4,5,6,7,8,9,10];
+var evenArr = [];
+for(i=1;i<=even.length;i++){
+    if(i%2 == 0){
+       evenArr.push(even[i-1]);
+    }
+}
+
+console.log("Even array:",...evenArr);
+
+
+/**
+ * Left rotate the array
+ */
+
+var left  = [1,2,3,4,5,6,7,8,9,10];
+
+let x = 3;
+for(i=0;i<x;i++){
+    let temp = left[0];
+
+    for(j=0;j<left.length;j++){
+        left[j] = left [j+1];
+    }
+
+    left[left.length-1] = temp;
+}
+
+console.log("Left rotate :",...left);
+
+/**
+ * Right rotate the array
+ */
+
+var right  = [1,2,3,4,5,6,7,8,9,10];
+
+let O = 3;
+for(i=0;i<O;i++){
+    let temp = right[right.length-1];
+
+    for(j=right.length-1;j>=0;j--){
+        right[j] = right[j-1];
+    }
+    right[0] = temp;
+}
+
+console.log("Right rotate :",...right)
--- Javascript programs/chair.js
+++ Javascript programs/chair.js
@@ -7,15 +7,15 @@ var temp = [];
 let i = n-1;
 let p = 0;
 
-while(i>n-k-1){             // 4>2 3>1 
+while(i>n-k-1){       //9>10-3-1      // 4>2 3>1 
     temp[p] = arr[i];      // temp[] = [4,5] 
     i--;
     p++;
 }
 
-let e = n-k-1; // 2 , 1 , 0
-while(e>=0){
-    arr[e+k] = arr[e]; //arr[4]=arr[2] arr[3]=arr[1] arr[2]=arr[0]
+let e = n-k-1; // 10-3-1
+while(e>=0){   //6>=0
+    arr[e+k] = arr[e]; //arr[9]=arr[6] arr[8]=arr[5] arr[7]=arr[4] arr[6]=arr[3] arr[5]=arr[2] arr[4]=arr[1] arr[3]=arr[0]
     e--;
 }
 
