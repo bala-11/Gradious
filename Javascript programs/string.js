3945e6c415ca41f9b470af000bae79e981f671c9
Balaji <balaji.kalimuthu@mphasis.com>
2023-12-09 22:20:39 +0530

    substring

--- Javascript programs/string.js
+++ Javascript programs/string.js
@@ -1,6 +1,6 @@
 /**
- *  count number of character  in a string 
-*/
+ *  count number of character  in a string
+ */
 
 var text = "balaji is a badass";
 // console.log(text.split(" ").join("").length);
@@ -8,60 +8,66 @@ var text = "balaji is a badass";
 /**
  * count vowels in a string
  */
-
 var str = text.toUpperCase();
 var vowels = 0;
 var consonants = 0;
-for(i=0;i<str.length;i++){
-    if(str[i] == "A" || str[i] == "E" || str[i] == "I" || str[i] == "O" || str[i] == "U" ){
-        vowels++;
-    }
-    else if(str[i] > "A" && str[i] < "Z" ){
-        consonants++;
-    } 
+for (i = 0; i < str.length; i++) {
+  if (
+    str[i] == "A" ||
+    str[i] == "E" ||
+    str[i] == "I" ||
+    str[i] == "O" ||
+    str[i] == "U"
+  ) {
+    vowels++;
+  } else if (str[i] > "A" && str[i] < "Z") {
+    consonants++;
+  }
 }
-
-console.log("Vowels count : ", vowels );
+console.log("Vowels count : ", vowels);
 console.log("Consonants count : ", consonants);
 
 /**
  * Anagram
  */
-
 var str1 = "SILENT";
 var str2 = "Listen";
 
-(()=>{
-    if(str1.length != str2.length){
-        console.log("Anagram not possible");
+(() => {
+  if (str1.length != str2.length) {
+    console.log("Anagram not possible");
+  } else {
+    let text1 = str1.split("").sort().join("");
+    let text2 = str2.split("").sort().join("");
+    if (text1.matchAll(text2)) {
+      console.log("These strings are AnaGram");
     }
-    else{
-        let text1 = str1.split("").sort().join("");
-        let text2 = str2.split("").sort().join("");
-        if(text1.matchAll(text2)){
-            console.log("These strings are AnaGram");
-        }
-    }
+  }
 })();
 
-
 /**
- * Substring 
+ * Substring
  */
 
-(()=>{
-    let str = "aaaabbbbccccdddd";
-    let n = 3 ;
-    let N = str.length;
-    let chars = N/n ;
-    if(str.length % 2 !=0){
-        console.log("Substring is not possible");
-    }
-    else{
-        let subStr = [];
-        for(i=0;i<N;i=i+chars){
-            subStr = str.substring(i,i+chars);
-            console.log(subStr);
-        }       
-    }
-})()
+let str4 = "aaaabbbbcccc";
+let n = 3;
+let N = str4.length;
+let chars = N / n;
+if (str4.length % n != 0) {
+  console.log("Substring is not possible");
+} else {
+  for (i = 0; i < N; i = i + chars) {
+    console.log(str4.substring(i, i + chars));
+  }
+}
+
+/**
+ * Total subsets
+ */
+
+let str3 = "BALAJI";
+for (i = 0; i < str3.length; i++) {
+  for (j = i; j < str3.length; j++) {
+    console.log(str3.substring(i, j+1));
+  }
+}
